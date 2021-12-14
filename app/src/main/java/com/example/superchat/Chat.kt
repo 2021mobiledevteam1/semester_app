daa2508502e9f90bfc86282c9ea0415943f9225d
Andrew Doepke <andrewdoepke@outlook.com>
2021-12-14 17:48:47 -0600

    doesnt work still but moved the channel display to chat.kt
    whatever

--- app/src/main/java/com/example/superchat/Chat.kt
+++ app/src/main/java/com/example/superchat/Chat.kt
@@ -2,11 +2,41 @@ package com.example.superchat
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
+import com.example.superchat.databinding.ActivityChatBinding
+import com.example.superchat.databinding.ActivitySignupBinding
+import io.getstream.chat.android.client.models.Filters
+import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModel
+import io.getstream.chat.android.ui.channel.list.viewmodel.bindView
+import androidx.activity.viewModels
+import io.getstream.chat.android.ui.channel.list.viewmodel.bindView
+import io.getstream.chat.android.ui.channel.list.viewmodel.factory.ChannelListViewModelFactory
 
 class Chat : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+        val binding = ActivityChatBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        // Step 3 - Set the channel list filter and order
+        // This can be read as requiring only channels whose "type" is "messaging" AND
+        // whose "members" include our "user.id"
+        val filter = Filters.and(
+            Filters.eq("type", "messaging"),
+            Filters.`in`("members", listOf(user.id))
+        )
+
+        val viewModelFactory =
+            ChannelListViewModelFactory(filter, ChannelListViewModel.DEFAULT_SORT)
+        val viewModel: ChannelListViewModel by viewModels { viewModelFactory }
+
+
+        // Step 4 - Connect the ChannelListViewModel to the ChannelListView, loose
+        //          coupling makes it easy to customize
+        viewModel.bindView(binding.channelListView, this)
+        binding.channelListView.setChannelItemClickListener { channel ->
+            startActivity(ChannelActivity.newIntent(this, channel))
+        }
     }
 }
--- app/src/main/java/com/example/superchat/MainActivity.kt
+++ app/src/main/java/com/example/superchat/MainActivity.kt
@@ -4,20 +4,15 @@ import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.widget.Toast
-import androidx.activity.viewModels
+
 import androidx.appcompat.app.AppCompatActivity
-import androidx.room.Dao
 import com.example.superchat.databinding.ActivityMainBinding
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.logger.ChatLogLevel
 import io.getstream.chat.android.client.models.Filters
 import io.getstream.chat.android.client.models.User
 import io.getstream.chat.android.livedata.ChatDomain
-import io.getstream.chat.android.ui.channel.list.viewmodel.ChannelListViewModel
-import io.getstream.chat.android.ui.channel.list.viewmodel.bindView
-import io.getstream.chat.android.ui.channel.list.viewmodel.factory.ChannelListViewModelFactory
-import androidx.room.Room
-import io.getstream.chat.android.client.api.models.QueryUsersRequest
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -57,17 +52,6 @@ class MainActivity : AppCompatActivity() {
                     token = token
                 ).enqueue {/* ... */}
 
-                // Step 3 - Set the channel list filter and order
-                // This can be read as requiring only channels whose "type" is "messaging" AND
-                // whose "members" include our "user.id"
-                val filter = Filters.and(
-                    Filters.eq("type", "messaging"),
-                    Filters.`in`("members", listOf(user.id))
-                )
-                val viewModelFactory =
-                    ChannelListViewModelFactory(filter, ChannelListViewModel.DEFAULT_SORT)
-                val viewModel: ChannelListViewModel by viewModels { viewModelFactory }
-
                 //store logged in prefs
                 pe.putBoolean("logged", true).apply()
                 pe.putString("currUser", user.id) //put user id into user prefs
@@ -75,14 +59,7 @@ class MainActivity : AppCompatActivity() {
                 print("Going to the channel List\n")
                 //TODO Connects just fine, now we need to get it to display channels
 
-                /*
-                // Step 4 - Connect the ChannelListViewModel to the ChannelListView, loose
-                //          coupling makes it easy to customize
-                viewModel.bindView(binding.channelListView, this)
-                binding.channelListView.setChannelItemClickListener { channel ->
-                    startActivity(ChannelActivity.newIntent(this, channel))
-                }
-                */
+                startActivity(Intent(this, Chat::class.java))
             }
         }
 
